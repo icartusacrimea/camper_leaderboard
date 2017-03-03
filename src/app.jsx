e34fa151ececa1d69626418755d4d5031d36bed7
icartusacrimea <icartusacrimea@gmail.com>
2017-03-02 22:43:31 -0500

    tidied code

--- src/app.jsx
+++ src/app.jsx
@@ -3,60 +3,64 @@ var ReactDOM = require('react-dom');
 var axios = require('axios');
 
 class App extends React.Component {
-	// makes sure the component is initialized properly
-	constructor(props) {
-		super(props);
-		this.state = {
-			pastThirtyDays: [],
-			allTime: [],
-			display: 'pastThirtyDays'
-		};
-	}
+  // makes sure the component is initialized properly
+  constructor(props) {
+    super(props);
+    this.state = {
+      pastThirtyDays: [],
+      allTime: [],
+      display: 'pastThirtyDays'
+    };
+  }
 
-	//use axios to quickly fetch APIs
-	componentWillMount() {
-		axios.all([this.getPastThirtyDays(), this.getAllTime()])
-			.then(axios.spread((pastThirtyDays, allTime) => {
-				this.setState({
-					pastThirtyDays: pastThirtyDays.data,
-					allTime: allTime.data
-				});
-			}));
-	}
+  //use axios to quickly fetch APIs
+  componentWillMount() {
+    axios.all([this.getPastThirtyDays(), this.getAllTime()])
+      .then(axios.spread((pastThirtyDays, allTime) => {
+        this.setState({
+          pastThirtyDays: pastThirtyDays.data,
+          allTime: allTime.data
+        });
+      }));
+  }
 
-	getPastThirtyDays() {
-		return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent');
-	}
+  getPastThirtyDays() {
+    return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent');
+  }
 
-	getAllTime() {
-		return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
-	}
+  getAllTime() {
+    return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
+  }
 
-	changeDisplay(display) {
-		this.setState({ display });
-	}
+  changeDisplay(display) {
+    this.setState({
+      display
+    });
+  }
 
-	render() {
-		return (
-	      <div>
+  render() {
+    return (
+      <div>
 			<h1>fCC Leaderboard</h1>
 			<button onClick={() => this.changeDisplay('pastThirtyDays')}>Past 30 Days</button>
 			<button onClick={() => this.changeDisplay('allTime')}>All Time</button>
 	        <Campers campers={this.state[this.state.display]} />
 	      </div>
-		);
-	}
+    );
+  }
 }
 
-const Campers = ({ campers }) => {
+const Campers = ({
+  campers
+}) => {
   var list = campers.map((camper, index) => {
     return (
-    	<eachCamper key={index} camper={camper} number={index + 1} />
+      <eachCamper key={index} camper={camper} number={index + 1} />
     );
   });
-	
+
   return (
-		<table>
+    <table>
 			<thead>
 				<tr>
 					<th>#</th>
@@ -69,19 +73,21 @@ const Campers = ({ campers }) => {
 				{list}
 			</tbody>
 		</table>
-	);
+  );
 }
 
-const eachCamper = ({ camper, number }) => {
-	return (
-	<tr>
+const eachCamper = ({
+  camper,
+  number
+}) => {
+  return (
+    <tr>
       <td>{number}</td>
       <td><a href={`https://freecodecamp.com/${camper.username}`} target="_blank">{camper.username}</a></td>
       <td>{camper.recent}</td>
       <td>{camper.alltime}</td>
     </tr>
-	);
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
-
