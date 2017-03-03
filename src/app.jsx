54691a42faf3030438e5eefb05a27657cdacf878
icartusacrimea <icartusacrimea@gmail.com>
2017-03-03 02:10:15 -0500

    component name changed to begin with capital letter/done!

--- src/app.jsx
+++ src/app.jsx
@@ -39,46 +39,52 @@ class App extends React.Component {
   }
 
   render() {
+    if (this.state.pastThirtyDays.length === 0 && this.state.allTime.length === 0) {
+      return (
+        <h2>Loading...</h2>
+      )
+    }
     return (
       <div>
 			  <h1>fCC Leaderboard</h1>
-			  <button onClick={() => this.changeDisplay('pastThirtyDays')}>Past 30 Days</button>
-			  <button onClick={() => this.changeDisplay('allTime')}>All Time</button>
-	      <Campers campers={this.state[this.state.display]} />
+        <div className="row">
+          <p>Sort by brownie points earned...</p>
+  			  <button onClick={() => this.changeDisplay('pastThirtyDays')}>Past 30 Days</button>
+  			  <button onClick={() => this.changeDisplay('allTime')}>All Time</button>
+  	      <Campers campers={this.state[this.state.display]} />
+        </div>
 	    </div>
     );
   }
 }
 
-const Campers = ({ campers }) => {
-  var list = campers.map((camper, index) => {
-    return (
-      <eachCamper key={index} camper={camper} number={index + 1} />
-    );
+const Campers = ({campers}) => {
+  const List = campers.map((camper, index) => {
+    return <EachCamper key={index} camper={camper} number={index + 1}/>
   });
 
   return (
-    <table>
+    <table className="table">
 			<thead>
 				<tr>
 					<th>#</th>
 					<th>Username</th>
-					<th>Points in Last 30 Days</th>
-					<th>All Time Points</th>
+					<th>Past 30 Days</th>
+					<th>All Time</th>
 				</tr>
 			</thead>
 			<tbody>
-				{list}
+        {List}
 			</tbody>
 		</table>
   );
 }
 
-const eachCamper = ({ camper, number }) => {
+const EachCamper = ({camper, number}) => {
   return (
     <tr>
       <td>{number}</td>
-      <td><a href={`https://freecodecamp.com/${camper.username}`} target="_blank">{camper.username}</a></td>
+      <td><a href={`https://freecodecamp.com/${camper.username}`} target="_blank"> <img src={camper.img}/> <span>{camper.username}</span></a></td>
       <td>{camper.recent}</td>
       <td>{camper.alltime}</td>
     </tr>
